edde1e2f80db773b37483b1e2ccd81322a3da3ac
leonardo oliveira <leonardo_olivesilva@icloud.com>
2023-06-11 15:23:17 -0700

    Linq Exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LINQ
+﻿using LINQ;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            { "Zelda", "Mario", "Street Fighter", "Mortal Kombat", "Alone in the Dark", "Resident Evil", "Star Wars","Harry Potter"};
+
+            var orderBy = videoGames.OrderBy(x => x.Length);
+            foreach (var game in orderBy)
+            {
+                Console.WriteLine($"{game}");
+            }
+
+           
+
+
         }
+
     }
 }
